30ba1d26e0e9af30452ae279dd3ed0866ec88027
Dima Ian <dima.ian.80@gmail.com>
2020-05-27 20:48:58 +0300

    Initial commit

---
+++ src/main/java/sweater/repos/MessageRepo.java
@@ -0,0 +1,13 @@
+package sweater.repos;
+
+
+import org.springframework.data.repository.CrudRepository;
+import sweater.domain.Message;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
